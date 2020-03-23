09138e2a6ab608e4c8045f01d96ac2accac509cc
root <root@30db770eff80>
2020-03-23 00:26:20 +0000

    dwasd

--- src/_10_cuteness_tv/Live_at_11.java
+++ src/_10_cuteness_tv/Live_at_11.java
@@ -2,35 +2,69 @@ package _10_cuteness_tv;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Live_at_11 implements ActionListener {
-	public static void main(String[] args) {
+    JButton Firstbutton = new JButton();
+    JButton Secondbutton = new JButton();
+    JButton Thirdbutton = new JButton();
+	void run() {
 		JFrame Firstframe = new JFrame();
 		JFrame Secondframe = new JFrame();
 		JFrame Thirdframe = new JFrame();
         Firstframe.setVisible(true);
         Secondframe.setVisible(true);
         Thirdframe.setVisible(true);
-        JButton Firstbutton = new JButton();
-        JButton Secondbutton = new JButton();
-        JButton Thirdbutton = new JButton();
         Firstbutton.setText("I have a cute video!");
         Secondbutton.setText("I also have a cute video!");
         Thirdbutton.setText("Me three!");
         Firstframe.add(Firstbutton);
         Secondframe.add(Secondbutton);
         Thirdframe.add(Thirdbutton);
+        Firstbutton.addActionListener(this);
+        Secondbutton.addActionListener(this);
+        Thirdbutton.addActionListener(this);
         Firstframe.pack();
         Secondframe.pack();
         Thirdframe.pack();
 	}
+	
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+	     try {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-
+		JButton ButtonClicked = (JButton) e.getSource();
+		if (ButtonClicked == Firstbutton) {
+			showDucks();
+		}
+		else if (ButtonClicked == Secondbutton) {
+			showFrog();
+		}
+		else if (ButtonClicked == Thirdbutton) {
+			showFluffyUnicorns();
+		}
 	}
 }
---
+++ src/_10_cuteness_tv/Live_at_11_Runner.java
@@ -0,0 +1,7 @@
+package _10_cuteness_tv;
+
+public class Live_at_11_Runner {
+public static void main(String[] args) {
+	new Live_at_11().run();
+}
+}
